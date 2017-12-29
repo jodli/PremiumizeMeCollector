258292980189e4880f37ef10e237ff8169bb355d
jodli <job87@web.de>
2017-12-29 03:25:31 +0100

    Implemented a function to check if the file was already added to the queue / collector. Finally added glue to add found files and links to the collector.

--- PremiumizeMeCollector.py
+++ PremiumizeMeCollector.py
@@ -3,6 +3,8 @@
 from ..internal.Addon import Addon
 from ..internal.misc import json
 
+import copy
+
 class PremiumizeMeCollector(Addon):
   __name__ = "PremiumizeMeCollector"
   __type__ = "hook"
@@ -22,6 +24,18 @@ class PremiumizeMeCollector(Addon):
 
   def activate(self):
     self.info['activated'] = True
+    self.packages = []
+
+    queue = self.pyload.api.getQueue()
+    self.log_debug(str(queue))
+    for package in queue:
+      self.packages.append(package.name)
+
+    collector = self.pyload.api.getCollector()
+    self.log_debug(str(collector))
+    for package in collector:
+      self.packages.append(package.name)
+
     self.log_debug('Activating with interval: {0} s'.format(self.config.get('interval')))
     self.periodical.start(self.config.get('interval'))
 
@@ -34,7 +48,13 @@ class PremiumizeMeCollector(Addon):
     self.log_debug('Running periodically...')
     #self.get_info(self.config.get('customerId'), self.config.get('pin'))
     files = self.get_files_in_folder(self.config.get('customerId'), self.config.get('pin'), "")
-    self.log_debug(str(files))
+    self.log_debug("files: " + str(files))
+    new_files = self.remove_duplicates(files)
+    self.log_debug("new_files: " + str(new_files))
+    
+    for file in new_files:
+      self.add_package(file['name'], file['link'])
+    
     #self.get_folder_entries(self.config.get('customerId'), self.config.get('pin'), "AMp43Zx6F1avhAyKJgyxlQ")
     #self.add_package("name123", "link123")
 
@@ -67,7 +87,7 @@ class PremiumizeMeCollector(Addon):
 
     result = self.api_respond("folder/list", user, password, params)
 
-    files = {}
+    files = []
     if result['status'] != "success":
       self.log_error("Call failed: " + result['message'])
     else:
@@ -79,13 +99,28 @@ class PremiumizeMeCollector(Addon):
         else:
           self.log_info("Found a file: " + entry['name'])
           self.log_info("with link: " + entry['link'])
-          files[entry['name']] = entry['link']
+          files.append({'name': entry['name'], 'link': entry['link']})
     return files
+
+  def remove_duplicates(self, files):
+    new_files = copy.deepcopy(files)
+    self.log_debug(str(new_files))
+
+    for f in files:
+      self.log_debug("Checking: " + str(f['name']))
+
+      if f['name'] in self.packages:
+        self.log_debug(str(f['name']) + " already added. Removing")
+        new_files.pop(f['name'])
+    return new_files
 
   def add_package(self, name, link):
     self.log_debug("Adding package with name: " + name)
     self.log_debug("and link: " + link)
 
     pid = self.pyload.api.addPackage(name, [link], 0)
+    if pid > 0:
+      self.packages.append(name)
+      self.log_debug(str(self.packages))
 
     self.log_debug("to collector. pid: " + str(pid))
