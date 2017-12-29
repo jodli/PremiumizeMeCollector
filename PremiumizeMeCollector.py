631b5af55abbe5ca073252b08d9f2180b9f6b33c
jodli <job87@web.de>
2017-12-29 14:57:41 +0100

    Fixed removing of package from list when it already exists. Cleanup for logs.

--- PremiumizeMeCollector.py
+++ PremiumizeMeCollector.py
@@ -26,13 +26,9 @@ class PremiumizeMeCollector(Addon):
     self.info['activated'] = True
     self.packages = []
 
-    queue = self.pyload.api.getQueue()
-    self.log_debug(str(queue))
-    for package in queue:
-      self.packages.append(package.name)
-
     collector = self.pyload.api.getCollector()
-    self.log_debug(str(collector))
+    self.log_debug("Initializing existing collector packages.")
+    #self.log_debug(str(collector))
     for package in collector:
       self.packages.append(package.name)
 
@@ -45,18 +41,14 @@ class PremiumizeMeCollector(Addon):
     self.periodical.stop()
 
   def periodical_task(self):
-    self.log_debug('Running periodically...')
-    #self.get_info(self.config.get('customerId'), self.config.get('pin'))
-    files = self.get_files_in_folder(self.config.get('customerId'), self.config.get('pin'), "")
-    self.log_debug("files: " + str(files))
+    #self.log_debug('Running periodically...')
+    files = self.get_files_in_folder(self.config.get('customerId'), self.config.get('pin'))
+    #self.log_debug("files: " + str(files))
     new_files = self.remove_duplicates(files)
-    self.log_debug("new_files: " + str(new_files))
+    #self.log_debug("new_files: " + str(new_files))
     
     for file in new_files:
       self.add_package(file['name'], file['link'])
-    
-    #self.get_folder_entries(self.config.get('customerId'), self.config.get('pin'), "AMp43Zx6F1avhAyKJgyxlQ")
-    #self.add_package("name123", "link123")
 
   def api_respond(self, method, user, password, params = None):
     if params is None:
@@ -67,16 +59,6 @@ class PremiumizeMeCollector(Addon):
 
     html = self.load(self.API_URL + method, get=params)
     return json.loads(html)
-
-  def get_info(self, user, password):
-    self.log_debug("Calling account/info")
-    
-    result = self.api_respond("account/info", user, password)
-
-    if result['status'] != "success":
-      self.log_error("Call failed: " + result['message'])
-    else:
-      self.log_debug("Result: " + str(result))
 
   def get_files_in_folder(self, user, password, folder_id = None):
     self.log_debug("Calling folder/list for id " + str(folder_id))
@@ -89,38 +71,38 @@ class PremiumizeMeCollector(Addon):
 
     files = []
     if result['status'] != "success":
-      self.log_error("Call failed: " + result['message'])
+      self.log_error("Call to folder/list failed: " + result['message'])
     else:
       self.log_debug("Result: " + str(result))
       for entry in result['content']:
-        self.log_debug(str(entry))
+        #self.log_debug(str(entry))
         if entry['type'] != "file":
-          self.log_debug("not a file.")
+          self.log_debug(entry['name'] + " is not a file. Skipping.")
         else:
-          self.log_info("Found a file: " + entry['name'])
-          self.log_info("with link: " + entry['link'])
+          self.log_debug("Found a file: " + entry['name'])
+          self.log_debug("with link: " + entry['link'])
           files.append({'name': entry['name'], 'link': entry['link']})
     return files
 
   def remove_duplicates(self, files):
     new_files = copy.deepcopy(files)
-    self.log_debug(str(new_files))
+    #self.log_debug(str(new_files))
 
     for f in files:
       self.log_debug("Checking: " + str(f['name']))
 
       if f['name'] in self.packages:
-        self.log_debug(str(f['name']) + " already added. Removing")
-        new_files.pop(f['name'])
+        self.log_debug(str(f['name']) + " already added. Removing.")
+        new_files.remove(f)
     return new_files
 
   def add_package(self, name, link):
-    self.log_debug("Adding package with name: " + name)
-    self.log_debug("and link: " + link)
+    self.log_info("Adding package with name: " + name)
+    self.log_info("and link: " + link)
 
     pid = self.pyload.api.addPackage(name, [link], 0)
     if pid > 0:
       self.packages.append(name)
-      self.log_debug(str(self.packages))
+      #self.log_debug(str(self.packages))
 
-    self.log_debug("to collector. pid: " + str(pid))
+    self.log_info("to collector. pid: " + str(pid))
